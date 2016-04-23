73e4916902b248be31e0624fde90e68e0d466e62
lee allen <leeallen337@gmail.com>
2016-04-23 17:41:41 -0400

    Making functions

--- src/js/init.js
+++ src/js/init.js
@@ -3,7 +3,7 @@ let inputs = document.querySelectorAll('input');
 let container = document.querySelector('.container');
 
 btn.addEventListener('click', function(el) {
-  let c = document.createDocumentFragment();
+  let windowWidth = window.innerWidth;
 
   for (let i = 0; i < inputs.length; i++) {
     if (!inputs[i].value) {
@@ -12,33 +12,11 @@ btn.addEventListener('click', function(el) {
     }
   }
 
-  if (container.children.length > 0) {
-    let fec = container.firstElementChild;
+  removeChildrenElements(container);
 
-    while (fec) {
-      container.removeChild(fec);
-      fec = container.firstElementChild;
-    }
-  }
+  buildGrid(container, inputs[0].value, inputs[1].value);
 
-  for (let i = 0; i < inputs[0].value; i++) {
-    let row = document.createElement('div');
-    row.classList.add('row');
-
-    for (let j = 0; j < inputs[1].value; j++) {
-      let box = document.createElement('div');
-      box.classList.add('grid-item');
-      row.appendChild(box);
-    }
-
-    c.appendChild(row);
-  }
-
-  for (let i = 0; i < inputs.length; i++) {
-    inputs[i].value = '';
-  }
-
-  document.querySelector('.container').appendChild(c);
+  resetInputs(inputs);
 
 });
 
@@ -47,3 +25,41 @@ container.addEventListener('mouseover', function(el) {
     el.target.classList.add('color');
   }
 });
+
+
+// Functions
+function removeChildrenElements(container) {
+  if (container.children.length > 0) {
+    let firstElementChild = container.firstElementChild;
+
+    while (firstElementChild) {
+      container.removeChild(firstElementChild);
+      firstElementChild = container.firstElementChild;
+    }
+  }
+}
+
+function buildGrid(parentElement, rowNumber, columnNumber) {
+  let fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < rowNumber; i++) {
+    let row = document.createElement('div');
+    row.classList.add('row');
+
+    for (let j = 0; j < columnNumber; j++) {
+      let item = document.createElement('div');
+      item.classList.add('grid-item');
+      row.appendChild(item);
+    }
+
+    fragment.appendChild(row);
+  }
+
+  parentElement.appendChild(fragment);
+}
+
+function resetInputs(inputsObj) {
+  for (let i = 0; i < inputsObj.length; i++) {
+    inputsObj[i].value = '';
+  }
+}
