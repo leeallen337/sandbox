24cde6b4de33c9a9d8ec701ce20f65a1803824e1
lee allen <leeallen337@gmail.com>
2016-05-07 13:57:12 -0400

    two way binding

--- src/js/init.js
+++ src/js/init.js
@@ -2,10 +2,5 @@ new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue.js'
-  },
-  methods: {
-    reverseMessage: function() {
-      this.message = this.message.split('').reverse().join('');
-    }
   }
 })
