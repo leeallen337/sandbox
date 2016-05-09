5969e0f7ef0c18936aa25dad16d5f872ea708e21
lee allen <leeallen337@gmail.com>
2016-05-08 20:20:36 -0400

    simplest possible example

--- src/js/init.js
+++ src/js/init.js
@@ -1,21 +1,8 @@
-new Vue({
-  el: '#app',
-  data: {
-    newTodo: '',
-    todos: [
-      { text: 'Add some todos' }
-    ]
-  },
-  methods: {
-    addTodo: function() {
-      var text = this.newTodo.trim();
-      if (text) {
-        this.todos.push({ text: text });
-        this.newTodo = '';
-      }
-    },
-    removeTodo: function(index) {
-      this.todos.splice(index, 1);
-    }
-  }
+var exampleData = {
+  name: 'Vue.js'
+}
+
+var exampleVM = new Vue({
+  el: '#example-1',
+  data: exampleData
 })
