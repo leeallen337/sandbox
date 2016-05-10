d04fb59bd849d1f18ba3508cf2567aa6adab7859
lee allen <leeallen337@gmail.com>
2016-05-09 21:36:27 -0400

    computed property setter

--- src/js/init.js
+++ src/js/init.js
@@ -5,8 +5,16 @@ var vm = new Vue({
     lastName: 'Bar'
   },
   computed: {
-    fullName: function() {
-      return this.firstName + ' ' + this.lastName;
+    fullName: {
+      get: function() {
+        return `${this.firstName} ${this.lastName}`
+      },
+      set: function(newValue) {
+        var names = newValue.split(' ');
+
+        this.firstName = names[0];
+        this.lastName = names[names.length - 1];
+      }
     }
   }
 })
