9888c1c0f21b52386b550f865d405d199c0151e4
lee allen <leeallen337@gmail.com>
2016-05-10 17:52:23 -0400

    binding html classes - object syntax

--- src/js/init.js
+++ src/js/init.js
@@ -1,20 +1,7 @@
 var vm = new Vue({
   el: '#demo',
   data: {
-    firstName: 'Foo',
-    lastName: 'Bar'
-  },
-  computed: {
-    fullName: {
-      get: function() {
-        return `${this.firstName} ${this.lastName}`
-      },
-      set: function(newValue) {
-        var names = newValue.split(' ');
-
-        this.firstName = names[0];
-        this.lastName = names[names.length - 1];
-      }
-    }
+    isA: true,
+    isB: false
   }
 })
