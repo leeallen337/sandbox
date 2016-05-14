677531609094ec5bb18ed0bd74c162cfb923bccc
lee allen <leeallen337@gmail.com>
2016-05-14 14:12:27 -0400

    inline statement handlers

--- src/js/init.js
+++ src/js/init.js
@@ -1,13 +1,8 @@
 var vm = new Vue({
   el: '#demo',
-  data: {
-    name: 'Vue.js'
-  },
   methods: {
-    greet: function(event) {
-      alert('Hello ' + this.name + '!');
-
-      alert(event.target.tagName);
+    say: function(msg) {
+      alert(msg);
     }
   }
 })
