c2222d433c1cee7948dbe2144c533ac57a342da0
lee allen <leeallen337@gmail.com>
2016-05-25 19:56:36 -0400

    dynamic options

--- src/js/init.js
+++ src/js/init.js
@@ -1,6 +1,11 @@
 var vm = new Vue({
   el: '#demo',
   data: {
-    checkedNames: []
+    selected: 'A',
+    options: [
+      { text: 'One', value: 'A' },
+      { text: 'Two', value: 'B' },
+      { text: 'Three', value: 'C' }
+    ]
   }
 })
