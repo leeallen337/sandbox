52dd58021b67af1d1c5b5dd888f4f1cf9108f678
Lee Allen <leeallen337@gmail.com>
2016-11-22 17:42:30 -0500

    Adding some CSS

--- src/js/init.js
+++ src/js/init.js
@@ -1,16 +1,7 @@
 let dataset = [ 5, 10, 15, 20, 25 ];
 
-d3.select('body').selectAll('p')
+d3.select('body').selectAll('div')
   .data(dataset)
   .enter()
-  .append('p')
-  .text(function (d) {
-    return d;
-  })
-  .style('color', function (d) {
-    if (d > 15) {
-      return 'red';
-    } else {
-      return 'blue';
-    }
-  });
+  .append('div')
+  .attr('class', 'bar');
