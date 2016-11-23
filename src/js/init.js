8e28677981506e7b55e2ca2147d5a5da692ffd4e
Lee Allen <leeallen337@gmail.com>
2016-11-23 13:28:15 -0500

    Testing out SVGs

--- src/js/init.js
+++ src/js/init.js
@@ -1,16 +1,4 @@
-let dataset = [];
+const svg = d3.select("body").append("svg");
 
-for (let i = 0; i < 25; i++) {
-  let randomNumber = Math.random() * 30;
-  dataset.push(randomNumber);
-}
-
-d3.select('body').selectAll('div')
-  .data(dataset)
-  .enter()
-  .append('div')
-  .attr('class', 'bar')
-  .style('height', function (d) {
-    let barHeight = d * 5;
-    return barHeight + 'px';
-  });
+svg.attr('width', 500)
+  .attr('height', 50);
