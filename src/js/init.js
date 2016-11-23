85e9d431c17925f976d260f2f4c01310589d34dd
Lee Allen <leeallen337@gmail.com>
2016-11-23 14:13:45 -0500

    Adding rectangles

--- src/js/init.js
+++ src/js/init.js
@@ -1,27 +1,18 @@
 const width = 500;
-const height = 50;
-const dataset = [ 5, 10, 15, 20, 25 ];
+const height = 100;
 
-const svg = d3.select("body")
-  .append("svg")
+const dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];;
+
+const svg = d3.select('body')
+  .append('svg')
   .attr('width', width)
   .attr('height', height);
 
-
-const circles = svg.selectAll('circle')
+svg.selectAll('rect')
   .data(dataset)
   .enter()
-  .append('circle');
-
-circles.attr('cx', function (d, i) {
-    return (i * 50) + 25;
-  })
-  .attr('cy', height / 2)
-  .attr('r', function (d) {
-    return d;
-  })
-  .attr('fill', 'yellow')
-  .attr('stroke', 'orange')
-  .attr('stroke-width', function (d) {
-    return d / 2;
-  });
+  .append('rect')
+  .attr('x', 0)
+  .attr('y', 0)
+  .attr('width', 20)
+  .attr('height', 100);
