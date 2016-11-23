295897b47cae3947874ec1680130cd787186375e
Lee Allen <leeallen337@gmail.com>
2016-11-23 13:29:20 -0500

    Assigning height and width to variables

--- src/js/init.js
+++ src/js/init.js
@@ -1,4 +1,6 @@
 const svg = d3.select("body").append("svg");
+const width = 500;
+const height = 50;
 
-svg.attr('width', 500)
-  .attr('height', 50);
+svg.attr('width', width)
+  .attr('height', height);
