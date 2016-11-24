d837267f46537ed3045b6ce02c733c830e80b1de
lee allen <leeallen@lees-MacBook-Pro.local>
2016-11-24 08:57:51 -0500

    Scatter plot

--- src/js/init.js
+++ src/js/init.js
@@ -1,7 +1,7 @@
 const width = 500;
 const height = 100;
 
-var dataset = [
+const dataset = [
   [ 5,     20 ],
   [ 480,   90 ],
   [ 250,   50 ],
@@ -13,3 +13,20 @@ var dataset = [
   [ 85,    21 ],
   [ 220,   88 ]
 ];
+
+const svg = d3.select('body')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height);
+
+svg.selectAll('circle')
+  .data(dataset)
+  .enter()
+  .append('circle')
+  .attr('cx', function (d) {
+    return d[0];
+  })
+  .attr('cy', function (d) {
+    return d[1];
+  })
+  .attr('r', 5);
