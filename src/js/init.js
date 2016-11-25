d6e2082edd0449f964c15553830e5616cd117899
lee allen <leeallen@lees-MacBook-Pro.local>
2016-11-25 08:08:11 -0500

    Building linear scale

--- src/js/init.js
+++ src/js/init.js
@@ -14,38 +14,6 @@ const dataset = [
   [ 220,   88 ]
 ];
 
-const svg = d3.select('body')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height);
-
-svg.selectAll('circle')
-  .data(dataset)
-  .enter()
-  .append('circle')
-  .attr('cx', function (d) {
-    return d[0];
-  })
-  .attr('cy', function (d) {
-    return d[1];
-  })
-  .attr('r', function (d) {
-    return Math.sqrt(height - d[1]);
-  });
-
-svg.selectAll('text')
-  .data(dataset)
-  .enter()
-  .append('text')
-  .text(function (d) {
-    return `${d[0]} , ${d[1]}`;
-  })
-  .attr('x', function (d) {
-    return d[0];
-  })
-  .attr('y', function (d) {
-    return d[1];
-  })
-  .attr('font-family', 'sans-serif')
-  .attr('font-size', '11px')
-  .attr('fill', 'red')
+const scale = d3.scaleLinear()
+  .domain([100, 500])
+  .range([10, 350]);
