149cd43beff670bfae55955f13bdec7fe9e2ba40
lee allen <leeallen@lees-MacBook-Pro.local>
2016-11-25 08:20:48 -0500

    Changing the range so it's not flipped

--- src/js/init.js
+++ src/js/init.js
@@ -14,6 +14,50 @@ const dataset = [
   [ 220,   88 ]
 ];
 
-const scale = d3.scaleLinear()
-  .domain([100, 500])
-  .range([10, 350]);
+const svg = d3.select('body')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height);
+
+const xScale = d3.scaleLinear()
+  .domain([0, d3.max(dataset, function (d) {
+    return d[0];
+  })])
+  .range([0, width]);
+
+const yScale = d3.scaleLinear()
+  .domain([0, d3.max(dataset, function (d) {
+    return d[1];
+  })])
+  .range([height, 0]);
+
+svg.selectAll('circle')
+  .data(dataset)
+  .enter()
+  .append('circle')
+  .attr('cx', function (d) {
+    return xScale(d[0]);
+  })
+  .attr('cy', function (d) {
+    return yScale(d[1]);
+  })
+  .attr('r', function (d) {
+    return Math.sqrt(height - d[1]);
+  })
+
+svg.selectAll('text')
+  .data(dataset)
+  .enter()
+  .append('text')
+  .text(function (d) {
+    return `${d[0]}, ${d[1]}}`;
+  })
+  .attr('x', function (d) {
+    return xScale(d[0]);
+  })
+  .attr('y', function (d) {
+    return yScale(d[1]);
+  })
+  .attr("font-family", "sans-serif")
+  .attr("font-size", "11px")
+  .attr("fill", "red");
