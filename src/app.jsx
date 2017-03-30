7d7d3fbbda5a6c57a885739cc13c17a11d6d61fb
Lee Allen <leeallen337@gmail.com>
2017-03-29 20:14:56 -0400

    Changing to switch and const

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,14 @@
 import expect from 'expect';
 import deepFreeze from 'deep-freeze';
 
-function counter(state, action) {
-  if (typeof state === 'undefined') {
-    return 0;
-  }
-
-  if (action.type === 'INCREMENT') {
-    return state + 1;
-  } else if (action.type === 'DECREMENT') {
-    return state - 1;
-  } else {
-    return state;
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
   }
 }
 
