50da67754fff29ac3974d710bc35c92777b8c0c1
Lee Allen <leeallen337@gmail.com>
2017-03-29 20:32:23 -0400

    Finishing building create store from scratch

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import expect from 'expect';
 import deepFreeze from 'deep-freeze';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 
 const counter = (state = 0, action) => {
   switch (action.type) {
@@ -11,6 +11,29 @@ const counter = (state = 0, action) => {
     default:
       return state;
   }
+}
+
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    }
+  };
+
+  dispatch({}); // return the initial value
+
+  return { getState, dispatch, subscribe };
 }
 
 const store = createStore(counter);
