e29aed57eeedba888794b6cdae12dc02f37b0de6
Lee Allen <leeallen337@gmail.com>
2017-04-03 11:11:31 -0400

    Finishing the video writing a todo list reducer (toggling a todo)

--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,6 @@ import deepFreeze from 'deep-freeze';
 import { createStore } from 'redux';
 
 const todos = (state = [], action) => {
-  
   switch (action.type) {
     case 'ADD_TODO':
       return [
@@ -16,6 +15,16 @@ const todos = (state = [], action) => {
           completed: false
         }
       ];
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if (todo.id !== action.id) {
+          return todo;
+        }
+
+        return Object.assign({}, todo, {
+          completed: !todo.completed
+        });
+      });
     default:
       return state;
   }
@@ -42,8 +51,45 @@ const testAddTodo = () => {
   expect(todos(stateBefore, action)).toEqual(stateAfter);
 }
 
+const testToggleTodo = () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: false
+    }
+  ];
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: true
+    }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(todos(stateBefore, action)).toEqual(stateAfter);
+}
+
 // //////
 
 testAddTodo();
+testToggleTodo();
 
 console.log('All tests passed.');
