89671d3aa01d477224c40520d0c7f8e2559cab67
Lee Allen <leeallen337@gmail.com>
2017-04-04 14:19:13 -0400

    Finishing video extracting action creators

--- src/app.jsx
+++ src/app.jsx
@@ -54,6 +54,29 @@ const todoApp = combineReducers({
   visibilityFilter
 });
 
+let nextTodoId = 0;
+const addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    id: nextTodoId++,
+    text
+  };
+};
+
+const setVisibilityFilter = (filter) => {
+  return {
+    type: 'SET_VISIBILITY_FILTER',
+    filter
+  }
+};
+
+const toggleTodo = (id) => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  };
+};
+
 // /////
 
 const getVisibleTodos = (todos, filter) => {
@@ -75,7 +98,7 @@ const mapStateToTodoListProps = (state) => {
 
 const mapDispatchToTodoListProps = (dispatch) => {
   return {
-    onTodoClick: (id) => dispatch({ type: 'TOGGLE_TODO', id })
+    onTodoClick: (id) => dispatch(toggleTodo(i))
   };
 };
 
@@ -113,8 +136,6 @@ const VisibleTodoList = connect(mapStateToTodoListProps, mapDispatchToTodoListPr
 
 // ///////
 
-let nextTodoId = 0;
-
 let AddTodo = ({ dispatch }) => {
   let input;
 
@@ -124,11 +145,7 @@ let AddTodo = ({ dispatch }) => {
         input = node;
       }} />
       <button onClick={() => {
-        dispatch({
-          type: 'ADD_TODO',
-          id: nextTodoId++,
-          text: input.value
-        });
+        dispatch(addTodo(input.value));
         input.value = '';
       }}> 
         Add Todo
@@ -149,7 +166,7 @@ const mapStateToLinkProps = (state, ownProps) => {
 
 const mapDispatchToLinkProps = (dispatch, ownProps) => {
   return {
-    onClick: () => dispatch({ type: 'SET_VISIBILITY_FILTER', filter: ownProps.filter })
+    onClick: () => dispatch(setVisibilityFilter(ownProps.filter))
   }
 };
 
