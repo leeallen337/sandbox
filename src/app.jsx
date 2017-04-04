b07a4a03dbaf82c7ccabd2b4d75e3fb810ddf9a4
Lee Allen <leeallen337@gmail.com>
2017-04-04 15:09:26 -0400

    Updates

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import HelloWorld from './components/HelloWorld';
+
+import styles from './css/index.css';
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('root')
+);
