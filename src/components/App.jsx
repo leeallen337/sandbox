70d971cba4a8885106198fa773c99ff799da8681
Lee Allen <leeallen337@gmail.com>
2017-04-05 14:59:33 -0400

    Cleaning up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,13 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter,
-  Route,
-  HashRouter,
-  MemoryRouter,
-  StaticRouter,
-  NativeRouter,
-  Link
-} from 'react-router-dom';
 
-const LinksRoutes = () => (
-  <div>
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
-    <Route exact path="/" render={() => (<h1>Home</h1>)} />
-    <Route path="/about" render={() => (<h1>About</h1>)} />
-  </div>
-)
-
-const forceRefresh = () => {
-  console.log(new Date());
-  return true;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
 }
 
-const BrowserRouterApp = () => (
-  <BrowserRouter forceRefresh={forceRefresh()}>
-    <LinksRoutes />
-  </BrowserRouter>
-)
-
-const HashRouterApp = () => (
-  <HashRouter hashType="slash">
-    <LinksRoutes />
-  </HashRouter>
-)
-
-const MemoryRouterApp = () => (
-  <MemoryRouter initialEntries={['/', '/about']} initialIndex={1}>
-    <LinksRoutes />
-  </MemoryRouter>
-)
-
-const StaticRouterApp = () => (
-  <StaticRouter location="/about" context={{}}>
-    <LinksRoutes />
-  </StaticRouter>
-)
-
-export default StaticRouterApp;
+export default App;
