dbbf9e6595482f0f39fe1d4352044386f8398f8e
Lee Allen <leeallen337@gmail.com>
2017-04-05 10:37:44 -0400

    Finishing the video render catch-all routes with rrv4 using the switch component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Link,
+  Switch
 } from 'react-router-dom';
 
 const Links = () => (
   <nav>
-    <Link to="/?id=123">Inline</Link>
-    <Link to={{pathname: '/', search: 'id=456'}}>Object</Link>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/contact">Contact</Link>
   </nav>
 );
 
@@ -16,14 +18,11 @@ const App = () => (
   <Router>
     <div>
       <Links />
-      <Route path="/" render={({ match, location }) => (
-        <div>
-          <p>root</p>
-          <p>{JSON.stringify(match)}</p>
-          <p>{JSON.stringify(location)}</p>
-          <p>{new URLSearchParams(location.search).get('id')}</p>
-        </div>
-      )} />
+      <Switch>
+        <Route exact path="/" render={() => <h1>Home</h1>} />
+        <Route path="/about" render={() => <h1>About</h1>} />
+        <Route render={() => <h1>Page not found</h1>} />
+      </Switch>
     </div>
   </Router>
 );
